7ceffc7a4faf48d7e1394a98f9cb487111e73f36
Rudi <rudi.will.do.it@gmail.com>
2023-04-02 13:25:30 +0300

    add cors config

---
+++ src/main/java/ru/rsreu/questionnaire/config/CorsWebConfig.java
@@ -0,0 +1,23 @@
+package ru.rsreu.questionnaire.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("/**",
+                                "/*",
+                                "*",
+                                "http://localhost:4200"
+                )
+                .allowCredentials(true);
+    }
+}
