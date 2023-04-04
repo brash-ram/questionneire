e5f29a5a9c15899b06baa0cbb85ce787b7b0eae5
Rudi <rudi.will.do.it@gmail.com>
2023-04-04 21:01:10 +0300

    init bd

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO presents (present_id, name, file, description, people_id)
+VALUES (1, 'Робот пылесос', '', 'https://clck.ru/343kj7', null),
+ (2, 'Микроволновка', '', 'https://clck.ru/343kjf', null),
+ (3, 'Кондиционер', '', 'https://clck.ru/343kjn', null),
+ (4, 'Стиральная машинка', '', 'https://clck.ru/343kjs', null),
+ (5, 'Телевизор', '', 'https://clck.ru/343kjy', null);
